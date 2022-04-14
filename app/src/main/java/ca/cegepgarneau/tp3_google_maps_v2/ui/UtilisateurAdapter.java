d41a45f2c92cb9f1084751b25fcdd255318ba0ac
Olivier Lamontagne <olamontagne@edu.cegepgarneau.ca>
2022-04-13 23:02:19 -0400

    Ajout layout one_line utilisateur

---
+++ app/src/main/java/ca/cegepgarneau/tp3_google_maps_v2/ui/UtilisateurAdapter.java
@@ -0,0 +1,17 @@
+package ca.cegepgarneau.tp3_google_maps_v2.ui;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import ca.cegepgarneau.tp3_google_maps_v2.R;
+import ca.cegepgarneau.tp3_google_maps_v2.model.Utilisateur;
+
+public class UtilisateurAdapter {
+}
